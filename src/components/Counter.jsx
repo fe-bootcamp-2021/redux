c25ae982fe473247285e153a5c71c1cd18d4da47
NAREK KARAPETYAN <krptnrkrptnrk@gmail.com>
2021-08-11 14:35:42 +0400

    first commit/ first task done

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,18 +1,60 @@
 import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment } from "../libs/store/features/counter";
+import { decrementByAmount, incrementByAmount, setStepValue, setMaxValue, setMinValue} from "../libs/store/features/counter";
+import "./Counter.css"
 
 export default function Counter() {
   const dispatch = useDispatch();
   const count = useSelector((state) => state.counter.value);
+let step = useSelector((state) => state.counter.step);
+let min = useSelector((state) => state.counter.min);
+let max = useSelector((state)=> state.counter.max)
 
-  const handleInc = () => {
-    dispatch(increment());
-  };
+const handleIncrementByStep = (step) => {
+  dispatch(incrementByAmount(step))
+}
+
+const handleDecrementByStep = (step) => {
+  dispatch(decrementByAmount(step))
+}
+
+const setStepAmount = (e) => {
+  dispatch(setStepValue(Number(e.target.value)))
+}
+
+const setMinAmount = (e) => {
+  dispatch(setMinValue(Number(e.target.value)))
+}
+
+const setMaxAmount = (e) => {
+  dispatch(setMaxValue(Number(e.target.value)))
+}
+
+// let disable = false;
+// if ((count + step) >= max ) {
+//   disable = !disable
+// }
 
   return (
-    <div>
-      {count}
-      <button onClick={handleInc}>+</button>
-    </div>
+    <div className="container">
+      <div><h1>{count}</h1></div>
+      <div>
+        <button disabled={count-step<min} onClick={() => handleDecrementByStep(step)}> - </button>
+        <button disabled={count+step>max} onClick={() => handleIncrementByStep(step)}> + </button>
+      </div>
+      <div>
+        <div>
+          <span>Step</span>
+        <input onChange={(e) => setStepAmount(e)} type="number" placeholder='1' />
+        </div>
+        <div>
+          <span>Min</span>
+        <input onChange={(e) => setMinAmount(e)} type="number" placeholder='-10' />
+        </div>
+        <div>
+          <span>Max</span>
+        <input onChange={(e) => setMaxAmount(e)} type="number" placeholder='10' />
+        </div>
+      </div>
+      </div>
   );
 }
