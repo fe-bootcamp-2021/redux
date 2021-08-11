b2b39f846420a3679ef009f0d6934cfa9686cf81
KarlNersisyan <karlennersisyan814@gmail.com>
2021-08-11 14:40:13 +0400

    feat(components,libs): Added min,max and step inputs

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,18 +1,57 @@
 import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment } from "../libs/store/features/counter";
+import { setStepValue,setMinValue,setMaxValue,decrementByAmount,incrementByAmount } from "../libs/store/features/counter";
+import classes from './Counter.module.css'
 
 export default function Counter() {
   const dispatch = useDispatch();
   const count = useSelector((state) => state.counter.value);
+  let step = useSelector((state) => state.counter.step)
+  let min = useSelector((state) => state.counter.min)
+  let max = useSelector((state) => state.counter.max)
 
-  const handleInc = () => {
-    dispatch(increment());
-  };
+  
+  const setStepAmount = (e) => {
+    dispatch(setStepValue(Number(e.target.value)))
+  }
+
+  const setMinAmount = (e) => {
+    dispatch(setMinValue(Number(e.target.value)))
+  }
+
+  const setMaxAmount = (e) => {
+    dispatch(setMaxValue(Number(e.target.value)))
+  }
+
+  const handleDecrementByStep = (step) => {
+    dispatch(decrementByAmount(step))
+  }
+  
+  const handleIncrementByStep = (step) => {
+    dispatch(incrementByAmount(step))
+  }
 
   return (
-    <div>
-      {count}
-      <button onClick={handleInc}>+</button>
+    <div className={classes.container}>
+      <div><h2> -Counter App with Redux- </h2></div>
+      <div className={classes.number}>{count}</div>
+      <div>
+        <button disabled={(count-step<min)} className={classes.pointer} onClick={() => handleDecrementByStep(step)}> - </button>
+        <button disabled={(count+step>max)} className={classes.pointer} onClick={() => handleIncrementByStep(step)}> + </button>
+      </div>
+      <div>
+        <div>
+          <span>Step</span>
+        <input onChange={(e) => setStepAmount(e)} type="number" placeholder='default/1' />
+        </div>
+        <div>
+          <span>Min</span>
+        <input  onChange={(e) => setMinAmount(e)} type="number" placeholder='default/(-10)' />
+        </div>
+        <div>
+          <span>Max</span>
+        <input  onChange={(e) => setMaxAmount(e)} type="number" placeholder='default/10' />
+        </div>
+      </div>
     </div>
   );
 }
--- src/libs/store/features/counter.js
+++ src/libs/store/features/counter.js
@@ -2,25 +2,34 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   value: 0,
+  step:1,
+  min:-10,
+  max:10,
 };
 
 export const counterSlice = createSlice({
   name: "counter",
   initialState,
   reducers: {
-    increment: (state) => {
-      state.value += 1;
-    },
-    decrement: (state) => {
-      state.value -= 1;
+    decrementByAmount: (state, action) => {
+      state.value -= action.payload;
     },
     incrementByAmount: (state, action) => {
       state.value += action.payload;
+    },
+    setStepValue: (state, action)=>{
+      state.step = action.payload;
+    },
+    setMinValue: (state, action)=>{
+      state.min = action.payload;
+    },
+    setMaxValue: (state, action)=>{
+      state.max = action.payload;
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { decrementByAmount,incrementByAmount, setStepValue,setMinValue,setMaxValue } = counterSlice.actions;
 
 export default counterSlice.reducer;
