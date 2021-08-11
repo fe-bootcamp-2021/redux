dda8b8a45e3a895d25aa953de32cf2dd745f54ef
Arthur <gyulabyana@gmail.com>
2021-08-11 22:26:01 +0400

    feat(Counter): add step, max and min input fields and change handlers

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,18 +1,52 @@
-import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment } from "../libs/store/features/counter";
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment, incrementByAmount, changeStep, changeMin, changeMax } from '../libs/store/features/counter';
 
 export default function Counter() {
   const dispatch = useDispatch();
   const count = useSelector((state) => state.counter.value);
+  const step = useSelector((state) => state.counter.step);
+  const min = useSelector((state) => state.counter.min);
+  const max = useSelector((state) => state.counter.max);
+  console.log(step);
 
   const handleInc = () => {
     dispatch(increment());
   };
 
+  const handleDec = () => {
+    dispatch(decrement());
+  };
+
+  const handleIncByAmount = () => {
+    dispatch(incrementByAmount(step));
+  };
+
+  const handleChange = (e, type) => {
+    if (type === 'step') {
+      dispatch(changeStep(Number(e.target.value)));
+    }
+
+    if (type === 'min') {
+      dispatch(changeMin(Number(e.target.value)));
+    }
+
+    if (type === 'max') {
+      dispatch(changeMax(Number(e.target.value)));
+    }
+  }
+
   return (
-    <div>
-      {count}
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <label htmlFor="step">Step:</label>
+      <input type="number" id="step" value={step} onChange={(e) => handleChange(e, 'step')} />
+      <label htmlFor="min">Min:</label>
+      <input type="number" id="min" value={min} onChange={(e) => handleChange(e, 'min')} />
+      <label htmlFor="max">Max:</label>
+      <input type="number" id="max" value={max} onChange={(e) => handleChange(e, 'max')} />
+      <span>{count}</span>
       <button onClick={handleInc}>+</button>
+      <button onClick={handleDec}>-</button>
+      <button onClick={handleIncByAmount}>+ By Amount</button>
     </div>
   );
 }
