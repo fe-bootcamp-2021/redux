a698479b38c284b1eaa771c5fac7b4577216cde2
Vladimir Hambardzumyan <vladimirhambardzumyan24@gmail.com>
2021-08-11 12:16:38 +0400

    feat(counter) : Added all functionality of counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,5 +1,11 @@
 import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment } from "../libs/store/features/counter";
+import {
+  minNum,
+  decrement,
+  increment,
+  maxNum,
+  step,
+} from "../libs/store/features/counter";
 
 export default function Counter() {
   const dispatch = useDispatch();
@@ -8,11 +14,27 @@ export default function Counter() {
   const handleInc = () => {
     dispatch(increment());
   };
+  const handleDec = () => {
+    dispatch(decrement());
+  };
+  const handleChangeMin = (e) => {
+    dispatch(minNum(Number(e.target.value)));
+  };
+  const handleChangeMax = (e) => {
+    dispatch(maxNum(Number(e.target.value)));
+  };
+  const incrementByAmount = (e) => {
+    dispatch(step(Number(e.target.value)));
+  };
 
   return (
     <div>
       {count}
+      <input onChange={handleChangeMin} placeholder="Min" />
+      <input onChange={handleChangeMax} placeholder="Max" />
+      <input onChange={incrementByAmount} placeholder="Step" />
       <button onClick={handleInc}>+</button>
+      <button onClick={handleDec}>-</button>
     </div>
   );
 }
--- src/libs/store/features/counter.js
+++ src/libs/store/features/counter.js
@@ -2,6 +2,9 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   value: 0,
+  min: 0,
+  max: 10,
+  step: 0
 };
 
 export const counterSlice = createSlice({
@@ -9,18 +12,31 @@ export const counterSlice = createSlice({
   initialState,
   reducers: {
     increment: (state) => {
-      state.value += 1;
+      if (state.value + state.step <= state.max) {
+        state.value += state.step;
+      }
+
     },
     decrement: (state) => {
-      state.value -= 1;
+      if (state.value - state.step >= state.min) {
+        state.value -= 1;
+      }
+
     },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload;
+    step: (state, action) => {
+      state.step = action.payload;
+    },
+    minNum: (state, action) => {
+      state.min = action.payload
+      state.value = action.payload
+    },
+    maxNum: (state, action) => {
+      state.max = action.payload
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { step, maxNum, minNum, increment, decrement, incrementByAmount } = counterSlice.actions;
 
 export default counterSlice.reducer;
